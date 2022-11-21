7066101bb0f5133caf160e5cce03825525ac1a3a
Thiago Fernandes <thiagomfernandes@msn.com>
2022-11-21 16:47:34 -0300

    Implementado busca em vários campos usando recursos django

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from . models import Contato
 from django.core.paginator import Paginator
 from django.http import Http404
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 # Create your views here.
 def index(request):
@@ -18,3 +20,21 @@ def detalhes(request, contato_id):
     if not contato or not contato.mostrar:
         raise Http404()
     return render(request, 'contatos/detalhes.html', {'contato' : contato}) #passa como json
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if not termo:
+        termo= ''
+
+    filtercampos = Concat('nome', Value(' '), 'sobrenome')
+    contatos = Contato.objects.order_by('-id').annotate(
+        nomesobre = filtercampos
+    ).filter(        
+        Q(nomesobre__icontains=termo) | Q(telefone__icontains=termo),
+        mostrar=True,
+    )
+    paginator = Paginator(contatos, 3)
+    page = request.GET.get('page')
+    contatos = paginator.get_page(page)
+    return render(request, 'contatos/busca.html', {'contatos' : contatos})
