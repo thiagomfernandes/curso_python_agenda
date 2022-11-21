01daa79ac903dc6f32b2e8518867e115c7355d73
Thiago Fernandes <thiagomfernandes@msn.com>
2022-11-21 18:14:37 -0300

    Implantado upload de imagem

--- agenda/settings.py
+++ agenda/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / 'templates' / 'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'upload'
+MEDIA_URL = 'upload/'
--- contatos/models.py
+++ contatos/models.py
@@ -14,10 +14,11 @@ class Contato(models.Model):
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
-    datacriacao = models.DateTimeField(default=timezone.now())
+    datacriacao = models.DateTimeField(default=timezone.now)
     descricao = models.CharField(max_length=255, blank=True)
     categoria = models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/')
     
     def __str__(self):
         return f'{self.nome} {self.sobrenome}'
